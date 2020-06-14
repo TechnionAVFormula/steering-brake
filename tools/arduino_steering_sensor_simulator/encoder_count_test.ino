d11aee60cb7187789d0630c590b96f9a5fe690ae
m-schwob <menashe54@gmail.com>
2020-06-14 11:10:11 +0300

    add arduino code that test the encoder counting

---
+++ tools/arduino_steering_sensor_simulator/encoder_count_test.ino
@@ -0,0 +1,45 @@
+#ifndef FILE_LOADED
+#define FILE_LOADED
+
+
+#include <Encoder.h>
+
+// define pins
+#define sensorA 2
+#define sensorB 3
+#define ledA 12
+#define ledB 11
+#define An1 A1
+#define An2 A2
+
+Encoder myEnc(sensorB, sensorA);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Basic Encoder Test:");
+}
+
+long oldPosition  = -999;
+unsigned long previousMillis = millis();
+bool position_printed = false;
+
+void loop() {
+    unsigned long currentMillis = millis();
+    long newPosition = myEnc.read();
+    if (newPosition != oldPosition ) {
+        oldPosition = newPosition;
+        previousMillis = currentMillis;
+        position_printed = false;
+    }
+    else {
+        if(currentMillis - previousMillis > 750 && !position_printed) {
+            position_printed = true;
+            Serial.println(newPosition);
+        }
+    }
+        
+}
+
+
+#endif //FILE_LOADED
